023f6bfea1fc874e189ad672fb9c1c2811caf903
Konstantin_Gladkiy <666xhot666@gmail.com>
2023-05-30 13:27:40 +0300

    Added a configuration file to store the necessary data for connecting to the database. Created a class to interact with this configuration file.

---
+++ AppConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Weather
+{
+    internal class AppConfiguration
+    {
+        public static IConfigurationRoot Configuration { get; }
+        static AppConfiguration()
+        {
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json",optional: false,reloadOnChange:true)
+            .Build();
+        }
+    }
+}
