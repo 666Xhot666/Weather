3b00a6d7e9e7a6b5386e0a2ff34d2d04acdec4c8
Konstantin_Gladkiy <666xhot666@gmail.com>
2023-05-31 19:25:17 +0300

    1) Added a class that represents collected statistics. 2) Added a class that represents a statistic collector.

---
+++ Data/MonthStatistic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Weather.Data
+{
+    public class MonthStatistic
+    {
+        public double TemperatureMean { get; set; }
+        public double PressureMean { get; set; }
+        public List<string>? Dates { get; set; }
+        
+    }
+}
---
+++ StatisticsCollector.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Weather.Data;
+
+namespace Weather
+{
+    internal class StatisticsCollector
+    {
+        private NotesDbContext dbContext;
+        public StatisticsCollector(string connectionString)
+        {
+            dbContext = new NotesDbContext(new DbContextOptionsBuilder<NotesDbContext>().UseMySQL(connectionString).Options);
+        }
+        public double CalculateAverageMonthlyTemperature(int month_id)
+        {
+            List<Notes> notes;
+            if (dbContext.LoadNotes(out notes))
+            {
+                var temperatures = notes.Where(n => n.month_id == month_id).Select(n => n.air_temperature);
+                if (temperatures.Any())
+                {
+                    return temperatures.Average();
+                }
+            }
+            return 0;
+        }
+
+        public double CalculateAverageMonthlyPressure(int month_id)
+        {
+            List<Notes> notes;
+            if (dbContext.LoadNotes(out notes))
+            {
+                var pressures = notes.Where(n => n.month_id == month_id).Select(n => n.pressure);
+                if (pressures.Any())
+                {
+                    return pressures.Average();
+                }
+            }
+            return 0;
+        }
+        public bool GetDatesWithNegativeTemperatureAndPrecipitation(out List<string> dates)
+        {
+            dates = new List<string>();
+            try
+            {
+                if (dbContext.LoadNotes(out List<Notes> notes))
+                {
+                    dates = notes
+                        .Where(n => n.air_temperature < 0 && n.presence_of_precipitation)
+                        .Select(n => $"{n.day.ToString("00")}/{n.month_id.ToString("00")}")
+                        .ToList();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
